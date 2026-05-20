9109be4637930f9ede857ba7cb14ac0b586379aa
subhadip978 <subhadipdas175@gmail.com>
2026-05-20 23:13:54 +0530

    feat:implement error handling on custome proxy

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -5,24 +5,26 @@ const httpProxy=require('http-proxy')
 
 
 
+const proxy = httpProxy.createProxyServer({});
+
 app.use((req,res)=>{
 		const  hostname=req.hostname;
 		const subdomain=hostname.split('.')[0] ;
 		// a1.localhost:8000 -->a1
-		const resolvesTo= `http://vercel-clone.s3.ap-south-1.amazonaws.com/_outputs/${subdomain}`;
+		const resolvesTo= `http://vercel-clone.s3.ap-south-1.amazonaws.com/__outputs/${subdomain}`;
 
-		Proxy.web(req,res,{target:resolvesTo,changeOrigin:true})
+		proxy.web(req,res,{target:resolvesTo,changeOrigin:true})
 })
 
-Proxy.on('proxyReq',(req,res)=>{
+proxy.on('proxyReq',(proxyReq, req, res)=>{
 	const url=req.url ;
 	if(url ==="/"){
-		proxyReq.path+=index.html ;
+		proxyReq.path+='index.html' ;
 	}
 })
 
 app.listen(PORT,()=>{
-	console.log(`reverse proxy is running at http://localhost:${port}`)
+	console.log(`reverse proxy is running at http://localhost:${PORT}`)
 })
 
 
